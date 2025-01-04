e05d0e15e7669028f0715c2e72b43da478316f7b
dale <dale@oneunivrs.com>
2025-01-04 12:47:45 +0900

    init commit

---
+++ src/main/java/com/yjh/whoplayer/WhoPlayErApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.whoplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoPlayErApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoPlayErApplication.class, args);
+    }
+
+}
